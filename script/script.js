28e43dab748aabc68d58ca9b7009bfddaa11cad0
Christofer Gabriel Pinheiro <48489750+Retr078@users.noreply.github.com>
2021-06-12 16:29:23 +0800

    everything good to go so far

---
+++ script/script.js
@@ -0,0 +1,59 @@
+
+// Background Animation
+const colors = ["#4285F4", "#DB4437", "	#C0C0C0", "#F4B400", "#0F9D58"];
+
+const numBalls = 500;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+ 
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+
+      
+    }
+  );
+}); 
+// Background Animation code ends here 
+
+// function to Toggle the password 
+
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+// function to Toggle the password ends here
