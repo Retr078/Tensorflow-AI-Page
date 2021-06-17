824309dd0ee7bfd1579f8b9f0fcdf38821777eab
Chris.G.P <cgpin78@outlook.com>
2021-06-17 17:45:41 +0800

    js changes

--- script/webcam_page.js
+++ script/webcam_page.js
@@ -3,13 +3,21 @@ var canvas = document.getElementById('canvas');
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({
-        video:true
-    }).then (function(stream){
+        video: true
+    }).then(function (stream) {
         video.srcObject = stream;
         video.play();
     });
 }
 var Context = canvas.getContext('2d');
-document.getElementById('snap').addEventListener('click', function(){
+document.getElementById('snap').addEventListener('click', function () {
     Context.drawImage(video, 0, 0, 640, 480);
+    var image = new Image();
+   var img = Context.toDataURL();
+    //document.getElementById('image_for_crop').appendChild(image);
+    console.log(img);
+    
 })
+
+
+//setInterval
